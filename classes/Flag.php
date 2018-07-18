32db9e3492be5bfe4e172fefb20b11972afee10b
Michael Alaimo <malaimo923@gmail.com>
2018-07-18 17:11:33 -0400

    Update to flag for cli scripts

--- classes/Flag.php
+++ classes/Flag.php
@@ -2,15 +2,21 @@
 
 namespace cli\classes;
 
-use common\object;
+use common\obj;
 use common\logging;
 
 
 /**
  * Should be extended when a cli command needs to support flags.
  */
-class Flag extends \common\object\Config {
+class Flag extends \common\obj\Config {
 	protected $prefix = '--';
+	
+	public function __construct() {
+	    if ($_SERVER['argc'] < 1) {
+	        throw new \ArgumentCountError('You must set CLI arguments: '.implode(', ', $this->getConfiguredParams()));
+	    }
+	}
 	
 	public function exchangeArray($arguments) {
 		if (!is_array($arguments)) {

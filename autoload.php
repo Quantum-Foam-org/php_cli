5cffb3b03c17b2e5acff62fb7fa23ae7639d19ec
Michael Alaimo <malaimo923@gmail.com>
2019-02-23 15:10:35 -0500

    Remove debug code

--- autoload.php
+++ autoload.php
@@ -6,9 +6,6 @@ spl_autoload_register(function($class) {
 	if (strpos($class, 'cli\\') === 0) {
 		$class = substr($class, 4);
 		$classFile = realpath(__DIR__.'/'.str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php');
-		if (strpos($classFile, 'trait') !== false) {
-		  echo $classFile."\n";
-		}
 		if (file_exists($classFile)) {
 			require($classFile);
 		}
